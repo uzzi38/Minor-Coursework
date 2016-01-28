d81bfea250c7f03eb38f5f73346f25703062b86e
ImmoZ <ImmoZ@128.86.155.160>
2016-01-28 05:29:10 +0000

    Done up to section 4

--- Minor-Coursework/src/usmankenichiro/Main.java
+++ Minor-Coursework/src/usmankenichiro/Main.java
@@ -1,7 +1,10 @@
 package usmankenichiro;
 
+import java.util.ArrayList;
+
 import usmankenichiro.tightlycoupled.Evidence;
 import usmankenichiro.tightlycoupled.EvidenceBox;
+import usmankenichiro.tightlycoupled.Policeman;
 
 public class Main {
 
@@ -14,6 +17,12 @@ public class Main {
 		box.add(powderResidue);
 		box.add(tireMarks);
 		System.out.println(box);
+		Policeman man1 = new Policeman("J. Lenk");
+		Policeman man2 = new Policeman("A. Colborn");
+		man1.addEvidenceBox(box);
+		ArrayList<Evidence> list = man1.possessedEvidences(2005000381, "DNA");
+		System.out.println("Pieces of DNA Evidence in Evidence Box number 2005000381 found by J. Lenk:");
+		System.out.println(list);
 	}
 
 }
--- Minor-Coursework/src/usmankenichiro/tightlycoupled/Policeman.java
+++ Minor-Coursework/src/usmankenichiro/tightlycoupled/Policeman.java
@@ -3,22 +3,35 @@ import java.util.ArrayList;
 
 public class Policeman {
 	private String name;
-	private ArrayList<EvidenceBox> evidenceBox;
-	public Policeman (String n) {name = n;}
+	private ArrayList<EvidenceBox> evidenceBoxes;
+	public Policeman (String n) {
+		name = n;
+		evidenceBoxes = new ArrayList<EvidenceBox>();
+	}
 	@Override
 	public String toString() {
 		return name;
 	}
-	
+	public void addEvidenceBox(EvidenceBox evidenceBox){
+		evidenceBoxes.add(evidenceBox);
+	}
 	public ArrayList<Evidence> possessedEvidences(int caseNumber, String type){
 		ArrayList<Evidence> result = new ArrayList<>();
-		for (EvidenceBox e : evidenceBox){
-			
-			
-		}
-		
+		for(EvidenceBox e1 : evidenceBoxes){
+			if(e1.getCaseNumber()== caseNumber){
+				ArrayList<Evidence> extract = e1.getEvidenceList();
+				for(Evidence e2 : extract){
+					if(e2.getType().equals(type)){
+						result.add(e2);
+					}	
+				}	
+			}
+		}		
+		return result;
 	}
-	
+
 
 }
+
+
 
