612a63c0bceae34c2f3cf2ac800124c9df95ac8a
uzzi38 <uzzi380@gmail.com>
2016-02-04 12:33:09 +0000

    Evidence JavaDoc notation created

--- Minor-Coursework/src/usmankenichiro/tightlycoupled/Evidence.java
+++ Minor-Coursework/src/usmankenichiro/tightlycoupled/Evidence.java
@@ -2,15 +2,26 @@ package usmankenichiro.tightlycoupled;
 
 
 public class Evidence {
-	
 	private final String type;
+	/**
+	 * Constructor for evidence. Stores the type of evidence inputed.
+	 * @param type of evidence.
+	 */
 	public Evidence(String type){
 		this.type = type;
 	}							//Evidence Constructor
+	/**
+	 * Customised toString method that returns the type of evidence.
+	 * @return type of evidence
+	 */
 	@Override
 	public String toString(){
 		return type;
 	}							//Personalised toString which returns the type of evidence
+	/**
+	 * Returns the type of Evidence for use in other classes.
+	 * @return Type of evidence
+	 */
 	public String getType() {
 		return type;
 	}							
